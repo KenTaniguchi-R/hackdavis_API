8995f03ae37ca70aa7c63b9f2c88920d1e9684f5
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2023-05-20 21:40:22 -0700

    Add comment count

--- posts/serializer.py
+++ posts/serializer.py
@@ -4,9 +4,14 @@ from .models import Post, Comment
 # Create your views here.
 
 class PostSerializer(serializers.ModelSerializer):
+
+    comment_count = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['id','content', 'timestamp', 'view_count']
+        fields = ['id','content', 'timestamp', 'view_count', 'comment_count']
+
+    def get_comment_count(self, obj):
+        return obj.comment_set.count()
 
 
 class CommentSerializer(serializers.ModelSerializer):
